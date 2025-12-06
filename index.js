83f601047b4ab5782f0c7c878d47340335de63ab
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-12-06 11:15:58 +0600

    Connected to mongoDB

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('AssetVesrse server  is running');
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tk04nnw.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. Successfully connected to MongoDB!");
+
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('AssetVerse server is running');
 })
 
-app.listen(port, () =>{
-    console.log(`AssetVesrse server is running on port: ${port}`);
+app.listen(port, () => {
+    console.log(`AssetVerse server is running on port: ${port}`);
 })
